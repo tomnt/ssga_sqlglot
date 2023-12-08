13e7a24f5cc105b21e08ee1b912af49727bfff5c
Tom S <tom_wrk@outlook.com>
2023-12-08 17:30:00 -0500

    Added the sample.sql file.

--- sql/input/sample.sql
+++ sql/input/sample.sql
@@ -1,5 +1,12 @@
-SELECT * FROM my_table_01;
+CREATE TABLE IF NOT EXISTS my_table (
+   my_int     integer not null,
+   my_num       numeric(18,8),
+   my_char_var           character varying(28)
+)
+DISTSTYLE KEY
+DISTKEY (SSGA_security_exchange_base_id);
+;
 
-SELECT * FROM my_table_02;
-
-SELECT * FROM my_table_03;
+--CREATE VIEW my_view AS
+--  SELECT my_num, my_char_var
+--  FROM my_table;
